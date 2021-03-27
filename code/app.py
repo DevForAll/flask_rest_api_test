ade54571371a3a9163d770c1a312841b0b261d53
Jhon Ochochoque Aguirre <jhon.ochochoque@alignet.com>
2021-03-27 17:58:37 -0500

    configuracion en archivo ini para que inicie la aplicacion y cambio en el archivo file para la creacion de la db

--- code/app.py
+++ code/app.py
@@ -19,10 +19,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key ='jose'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-	db.create_all()
-
 #	 CREA UN NUEVO ENDPOINT "/auth"
 jwt = JWT(app, authenticate, identity)
 
