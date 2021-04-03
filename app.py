c83ed12394ceb3771e239d2a4ce769a7751131e5
Jhon Ochochoque Aguirre <jhon.ochochoque@alignet.com>
2021-04-02 19:34:29 -0500

    agregando packages, se agrego la funcion para crear la base de datos, usando JWT_EXTENDED y para agregar valores en la respuesta del JSON y poder indentificarlos, agregando 2 endpoints y editando 1 endpoint para el USUARIO

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import os
 from flask import Flask
 from flask_restful import Api
-from flask_jwt import JWT
-
-from security import authenticate, identity
-from resources.user import UserRegister
+from flask_jwt_extended import JWTManager
+from db import db
+from resources.user import UserRegister, User, UserLogin
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
+
 
 
 app = Flask(__name__)
@@ -15,21 +15,35 @@ app.config['DEBUG'] = True
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#   LEVANTA LAS EXCEPCIONES PROPIAS, CODIGO Y MENSAJES DEL ERROR ESPECIFICO
+app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key ='jose'
 api = Api(app)
 
-#	 CREA UN NUEVO ENDPOINT "/auth"
-jwt = JWT(app, authenticate, identity)
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+#	 VINCULANDO EL JWT CON LA APP
+jwt = JWTManager(app)
+
+@jwt.additional_claims_loader
+def add_claims_to_jwt(identity):    #   FUNCION PARA AGREGAR DATOS EXTRA EN LA RESPUESTA DEL TOKEN
+    if identity == 1:   # El "1" es el ID del usuario que se creo con la BD sqlite
+        return {'is_admin': True}
+    return {'is_admin': False}
 
 #	AGREGANDO EL RECURSO "STUDENT" 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList,'/items')
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
-api.add_resource(UserRegister, '/register')
+api.add_resource(UserRegister, '/user/register')
+api.add_resource(User, '/user/<int:user_id>')
+api.add_resource(UserLogin, '/login')
 
 #	SOLO SE VA EJECUTAR EL ARCHIVO app.py
 if __name__ == '__main__':
-    from db import db
+
     db.init_app(app)
     app.run(port=5000, debug=True)
