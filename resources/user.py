6e5972b918564fa82859283fe0e99b34c444c599
Jhon Ochochoque Aguirre <jhon.ochochoque@alignet.com>
2021-04-04 13:19:01 -0500

    se agrego un Endpoint para refrescar el token y un Endpoint para desloguearse

--- resources/user.py
+++ resources/user.py
@@ -1,8 +1,16 @@
 import sqlite3
 from flask_restful import Resource, reqparse
 from werkzeug.security import safe_str_cmp
-from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_jwt_extended import (
+    create_access_token,
+    create_refresh_token,
+    jwt_required,
+    get_jwt_identity,
+    get_jwt
+    )
 from models.user import UserModels
+from blacklist import BLACKLIST
+
 
 _user_parse = reqparse.RequestParser()
 _user_parse.add_argument('username',
@@ -30,6 +38,7 @@ class UserRegister(Resource):
 
         return {'message': 'User register successfully'}, 201
 
+
 class User(Resource):
     @classmethod
     def get(cls, user_id):
@@ -46,6 +55,7 @@ class User(Resource):
         
         user.delete_from_db()
         return {'message': 'User deleted'}, 200
+
 
 class UserLogin(Resource):
     @classmethod
@@ -68,3 +78,18 @@ class UserLogin(Resource):
             }, 200
         
         return {'message': 'Invalid credentials'}, 401
+
+class UserLogout(Resource):
+    @jwt_required()
+    def post(self):
+        jti = get_jwt()['jti'] #   jti es "JWT ID", un identificador unico para un JWT
+        BLACKLIST.add(jti)
+        return {'message': 'Se desconectó correctamente'}, 200
+
+
+class TokenRefresh(Resource):
+    @jwt_required(refresh=True) #   CON ESTE DECORADOR SE VA RECIBIR UN TOKEN DE ACTUALIZACION
+    def post(self):
+        current_user = get_jwt_identity()   #Obteniendo la identidad del usuario
+        new_token = create_access_token(identity=current_user, fresh=False)
+        return {'access_token': new_token,}, 200
